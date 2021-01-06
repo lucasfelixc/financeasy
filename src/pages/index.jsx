16b21fe25cf33ec230cf34f6e47d62a254e6d703
Lucas Felix <lucasfelixdev@gmail.com>
2021-01-05 21:37:46 -0300

    (feat): finalization of the mobile header structure

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import Head from 'next/head'
 
+import Perfil from '../assets/images/lucas.svg'
+
+import { Container, ContentHeader } from '../styles/pages/Home/styles'
+
 export default function Home() {
+  const name = 'Lucas Felix'
+
+  function cutName(fullName) {
+    const firstName = fullName.split(' ', 2)
+
+    return firstName[0]
+  }
+
   return (
-    <div>
+    <Container>
       <Head>
         <title>Financeasy</title>
       </Head>
 
-      <main>
-        <h1>Hello world</h1>
-      </main>
-    </div>
+      <ContentHeader>
+        <div className="contentInfoPerfil">
+          <strong>Olá {cutName(name)}</strong>
+          <span>Bem vindo de volta</span>
+        </div>
+
+        <div className="contentImgPerfil">
+          <Perfil />
+        </div>
+      </ContentHeader>
+
+      <main></main>
+    </Container>
   )
 }
---
+++ src/styles/pages/Home/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+export const ContentHeader = styled.header`
+  width: 100vw;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .contentInfoPerfil {
+    padding: 20px 0 0 20px;
+
+    display: flex;
+    flex-direction: column;
+  }
+
+  .contentInfoPerfil strong {
+    font-size: 20px;
+  }
+
+  .contentInfoPerfil span {
+    font-size: 10px;
+    color: #6d749e;
+  }
+
+  .contentImgPerfil {
+    padding: 20px 20px 0 0;
+  }
+
+  .contentImgPerfil svg {
+    width: 50px;
+    height: 50px;
+
+    border-radius: 50% 50%;
+  }
+`
