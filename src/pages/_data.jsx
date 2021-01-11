c3da8f6003b8b83295645aa3a595eff420acf702
Lucas Felix <lucasfelixdev@gmail.com>
2021-01-10 22:29:19 -0300

    (feat): start of spend history analysis page

---
+++ src/pages/_data.jsx
@@ -0,0 +1,57 @@
+export const users = [
+  {
+    name: 'Lucas Felix',
+    balance: '$ 18.468,00',
+    spending: '$ 13.452,00'
+  }
+]
+
+export const cards = [
+  {
+    name: 'Nubank',
+    number: '4465 1122 3562 8751',
+    type: 'Mastercard',
+    background: '#8A05BE'
+  },
+  {
+    name: 'Inter',
+    number: '4465 1122 3562 6524',
+    type: 'Mastercard',
+    background: '#FF7A00'
+  },
+  {
+    name: 'Inter',
+    number: '4465 1122 3562 6524',
+    type: 'Mastercard',
+    background: '#FF7A00'
+  },
+  {
+    name: 'Inter',
+    number: '4465 1122 3562 6524',
+    type: 'Mastercard',
+    background: '#FF7A00'
+  }
+]
+
+export const payments = [
+  {
+    name: 'Salário',
+    infoSecondary: 'Fixo',
+    value: '$2.000,00'
+  },
+  {
+    name: 'Paypal',
+    infoSecondary: 'Freelance',
+    value: '$45,00'
+  },
+  {
+    name: 'Paypal',
+    infoSecondary: 'Freelance',
+    value: '$45,00'
+  },
+  {
+    name: 'Paypal',
+    infoSecondary: 'Freelance',
+    value: '$45,00'
+  }
+]
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,62 +15,7 @@ import {
   InfoPay
 } from '../styles/pages/Home/styles'
 
-const users = [
-  {
-    name: 'Lucas Felix',
-    balance: '$ 10,00'
-  }
-]
-
-const cards = [
-  {
-    name: 'Nubank',
-    number: '4465 1122 3562 8751',
-    type: 'Mastercard',
-    background: '#8A05BE'
-  },
-  {
-    name: 'Inter',
-    number: '4465 1122 3562 6524',
-    type: 'Mastercard',
-    background: '#FF7A00'
-  },
-  {
-    name: 'Inter',
-    number: '4465 1122 3562 6524',
-    type: 'Mastercard',
-    background: '#FF7A00'
-  },
-  {
-    name: 'Inter',
-    number: '4465 1122 3562 6524',
-    type: 'Mastercard',
-    background: '#FF7A00'
-  }
-]
-
-const payments = [
-  {
-    name: 'Salário',
-    infoSecondary: 'Fixo',
-    value: '$2.000,00'
-  },
-  {
-    name: 'Paypal',
-    infoSecondary: 'Freelance',
-    value: '$45,00'
-  },
-  {
-    name: 'Paypal',
-    infoSecondary: 'Freelance',
-    value: '$45,00'
-  },
-  {
-    name: 'Paypal',
-    infoSecondary: 'Freelance',
-    value: '$45,00'
-  }
-]
+import { users, cards, payments } from './_data'
 
 export default function Home() {
   function cutName(fullName) {
